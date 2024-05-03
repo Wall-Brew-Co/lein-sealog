9d8afdcab9031c3a94d097714d4b71293308f4fb
Nick Nichols <nichols1991@gmail.com>
2024-05-03 12:18:52 +0000

    Use sealog for version info, assert good sealog state

--- project.clj
+++ project.clj
@@ -10,15 +10,13 @@
   :dependencies [[com.wallbrew/spoon "1.2.2"]
                  [java-time-literals/java-time-literals "2018-04-06"]
                  [metosin/spec-tools "0.10.6"]
-                 [org.clojure/clojure "1.11.3"]
-                 [org.clojure/core.match "1.1.0"]
-                 [org.clojure/test.check "1.1.1"]]
+                 [org.clojure/clojure "1.11.3"]]
   :plugins [[com.github.clj-kondo/lein-clj-kondo "2024.03.13"]
-            [com.wallbrew/lein-sealog "1.4.0"]
+            [com.wallbrew/lein-sealog "1.5.0"]
             [lein-cljsbuild "1.1.8"]
-            [lein-project-version "0.1.0"]
             [mvxcvi/cljstyle "0.16.630"]
             [ns-sort "1.0.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]]}}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                     :username      :env/clojars_user
                                     :password      :env/clojars_pass
