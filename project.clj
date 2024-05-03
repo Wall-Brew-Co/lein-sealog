b6cd99def60b3a8d4d5c861a8c4ad358aefc65e3
Nick Nichols <nichols1991@gmail.com>
2024-05-03 12:33:18 +0000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wallbrew/lein-sealog "1.5.0"
+(defproject com.wallbrew/lein-sealog "1.6.0"
   :description "A Leiningen plugin for managing your changelog."
   :url "https://github.com/Wall-Brew-Co/common-beer-format"
   :license {:name         "MIT"
