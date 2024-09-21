31ce3c975205b4a4e2f213e5b7a24cf60b9e9c56
Nick Nichols <nichols1991@gmail.com>
2024-09-21 16:27:34 +0000

    Install bouncer

--- project.clj
+++ project.clj
@@ -7,15 +7,18 @@
             :comments     "Same-as all Wall-Brew projects"}
   :scm {:name "git"
         :url  "https://github.com/Wall-Brew-Co/lein-sealog"}
+  :pom-addition [:organization
+                 [:name "Wall Brew Co."]
+                 [:url "https://wallbrew.com"]]
   :dependencies [[com.wallbrew/spoon "1.2.3"]
                  [java-time-literals/java-time-literals "2018-04-06"]
                  [metosin/spec-tools "0.10.7"]
                  [org.clojure/clojure "1.12.0"]]
   :plugins [[com.github.clj-kondo/lein-clj-kondo "2024.08.29"]
+            [com.wallbrew/bouncer "1.0.0"]
             [com.wallbrew/lein-sealog "1.6.0"]
-            [lein-cljsbuild "1.1.8"]
             [mvxcvi/cljstyle "0.16.630"]
-            [ns-sort "1.0.3"]]
+            [ns-sort/ns-sort "1.0.3"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]]}}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                     :username      :env/clojars_user
