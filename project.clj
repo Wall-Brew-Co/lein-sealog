0efbc0dcd02738e1e61b2b0fad44a53354eb68bc
Nick Nichols <nichols1991@gmail.com>
2024-09-21 11:29:26 -0500

    Dogfood bouncer (#97)
    * Install bouncer
    
    * Update changelog

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wallbrew/lein-sealog "1.6.0"
+(defproject com.wallbrew/lein-sealog "1.7.0"
   :description "A Leiningen plugin for managing your changelog."
   :url "https://github.com/Wall-Brew-Co/common-beer-format"
   :license {:name         "MIT"
@@ -7,15 +7,18 @@
             :comments     "Same-as all Wall-Brew projects"}
   :scm {:name "git"
         :url  "https://github.com/Wall-Brew-Co/lein-sealog"}
+  :pom-addition [:organization
+                 [:name "Wall Brew Co."]
+                 [:url "https://wallbrew.com"]]
   :dependencies [[com.wallbrew/spoon "1.2.3"]
                  [java-time-literals/java-time-literals "2018-04-06"]
                  [metosin/spec-tools "0.10.7"]
                  [org.clojure/clojure "1.12.0"]]
   :plugins [[com.github.clj-kondo/lein-clj-kondo "2024.08.29"]
+            [com.wallbrew/bouncer "1.0.0"]
             [com.wallbrew/lein-sealog "1.6.0"]
-            [lein-cljsbuild "1.1.8"]
             [mvxcvi/cljstyle "0.16.630"]
-            [ns-sort "1.0.3"]]
+            [ns-sort/ns-sort "1.0.3"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]]}}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                     :username      :env/clojars_user
